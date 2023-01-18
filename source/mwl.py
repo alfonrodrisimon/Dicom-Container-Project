f24040b6653b3f24bc5d401681f38bacecfa048e
Rodríguez <RODRIAL1@Alcon.net>
2023-01-18 11:14:38 +0100

    Created function build_mwl_entry

--- source/mwl.py
+++ source/mwl.py
@@ -1,9 +1,13 @@
+import os, sys
+
 # Import libraries
+from pydicom.charset import encode_string
 from pydicom.dataset import Dataset, FileMetaDataset
 from pydicom.uid import ExplicitVRLittleEndian
 from pynetdicom import AE, evt
 from pynetdicom.sop_class import ModalityWorklistInformationFind
-from datetime import date
+from datetime import datetime
+from pydicom.uid import generate_uid
 
 # Implement the handler for evt.EVT_C_FIND
 def handle_find(event):
@@ -11,36 +15,51 @@ def handle_find(event):
      """Handle a C-FIND request event."""
      ds = event.identifier
 
-     # Create a MWL entry
-     mwl = Dataset()
+     # Get the query keys 
+     # query = ( elem for elem in ds.iterall() if not elem.is_empty ) # generator expression
 
-    # Add file meta information elements
-     mwl.file_meta = FileMetaDataset()
-     mwl.file_meta.TransferSyntaxUID = ExplicitVRLittleEndian
-     mwl.file_meta.MediaStorageSOPClassUID = "1.2.276.0.7230010.3.1.0.1"
-     mwl.file_meta.MediaStorageSOPInstanceUID = "1.2.276.0.7230010.3.1.4.2831176407.11154.1448031138.805061"
-     mwl.file_meta.ImplementationClassUID = "1.2.276.0.7230010.3.0.3.6.0"
-     mwl.file_meta.ImplementationVersionName = "OFFIS_DCMTK_360"
+     # Generate response payload
+     response = Dataset()
 
-    # Add data elements
-     #mwl.SpecificCharacterSet             = "ISO_IR 100"
-     mwl.AccessionNumber                  = "123456789"
-     mwl.PatientName                      = "MARÍA^LÓPEZ"
-     mwl.PatientID                        = "987654321"
-     mwl.OtherPatientIDs                  = "111111"
-     mwl.PatientBirthDate                 = "19700101"
-     mwl.PatientSex                       = "M"
-     mwl.StudyInstanceUID                 = "1.2.276.0.75.2.1.11.1.1.1667387252720.80" 
-     mwl.RequestedProcedureDescription    = "Ophthalmic examination and evaluation"
-     mwl.ScheduledProcedureStepSequence   = [Dataset()]
-     mwl.ScheduledProcedureStepSequence[0].Modality                           = "OP"
-     mwl.ScheduledProcedureStepSequence[0].ScheduledStationAETitle            = "ARGOS"
-     mwl.ScheduledProcedureStepSequence[0].ScheduledProcedureStepStartDate    = "20230110"
-     mwl.ScheduledProcedureStepSequence[0].ScheduledProcedureStepStartTime    = "080000"
-     mwl.ScheduledProcedureStepSequence[0].ScheduledPerformingPhysicianName   = "Doctor"
+     
 
-     # Pending
-     yield (0xFF00, mwl)
+     #for elem in response.iterall():
+      
+      #response.clear(elem.tag)
+     
+     # for elem in query:
+         # print(elem)
+
+     # Create instances
+     instances = []
+
+     def build_mwl_entry():
+        
+        ds = Dataset()
+        
+        ds.SpecificCharacterSet = 'ISO_IR 100'
+        ds.AccessionNumber = '09876543'
+        ds.PatientName = 'MARÍA^LÓPEZ'
+        ds.PatientID = '123456'
+        ds.PatientBirthDate = '20000101'
+        ds.PatientSex = 'M'
+        ds.StudyInstanceUID = generate_uid()
+        ds.RequestedProcedureDescription = 'Ophthalmic examination and evaluation'
+
+        # Performed Procedure Step Relationship
+        ds.ScheduledProcedureStepSequence = [Dataset()]
+        step_seq = ds.ScheduledProcedureStepSequence
+        step_seq[0].Modality = 'OP'
+        step_seq[0].ScheduledStationAETitle = 'ARGOS'
+        step_seq[0].ScheduledProcedureStepStartDate = datetime.now()
+        step_seq[0].ScheduledProcedureStepStartTime= "0900"
+
+        return ds
+
+     response = build_mwl_entry()
+
+     # Return all matches
+     yield (0xFF00, response)
 
 handlers = [(evt.EVT_C_FIND, handle_find)]
 
